593c8193854fb20f1e57fd9693592d97eda953b7
siyamrh7 <siyamrh7@gmail.com>
2021-07-03 23:31:07 +0600

    make it better

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const app=express()
 app.use(cors())
 app.use(express.json())
 app.use('/',router)
+app.get('/',(req,res)=>{
+    res.send("server okay")
+})
 app.listen(2000,()=>{
     console.log("server runnuing at 20000")
 })
